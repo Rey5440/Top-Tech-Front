e74705ff1cd390c3a30ea3fac2abdb0e76f968f8
F.Fernandez <hellsingsd@gmail.com>
2023-12-19 01:19:42 -0300

    la nueva

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+import dotenv from 'dotenv';
+dotenv.config();
+
+//importrar variables de entorno de para domain y clientId
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider  domain="dev-mxntk40w42hj7fze.us.auth0.com" clientId="R8Si6nJwh5Kv1yPl3FEqWaJDV8hHsPyL">
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
