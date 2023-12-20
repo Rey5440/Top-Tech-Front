4fc8c5bed7cb7dd21bcc7302267d1a83a8835b76
F.Fernandez <hellsingsd@gmail.com>
2023-12-20 16:18:33 -0300

    trayendo dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'; // Asumiendo que estás utilizando react-router-dom
 import Home from './components/home';
+import Turnos from './components/turnos';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/turnos" element={<Turnos/>} />
       </Routes>
     </div>
   )
--- src/components/turnos.jsx
+++ src/components/turnos.jsx
@@ -1,11 +1,20 @@
+import React from 'react';
+import Calendar from './calendar';
 
-const Turnos = () =>{
-
-    return (
-        <div>
-            <h1>este es el componente turnos</h1>
-        </div>
-    )
-}
+const Turnos = () => {
+  return (
+    <div>
+      <select id="servicio" name="servicio" defaultValue="Seleccione un servicio">
+        <option value="Seleccione un servicio" disabled>
+          Seleccione un servicio
+        </option>
+        <option value="corte">
+          corte
+        </option>
+        {/* Aquí puedes agregar más opciones de servicios si es necesario */}
+      </select>
+    </div>
+  );
+};
 
 export default Turnos;
